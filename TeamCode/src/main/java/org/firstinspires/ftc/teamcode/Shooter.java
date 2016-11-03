47b7242e446dcaa1bcb17b00f11d1c899bd89818
jas <jas>
2016-11-03 07:04:38 -0400

    added some logic that I thought about over night

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Hardware;
 
 
 public class Shooter {
@@ -10,38 +11,64 @@ public class Shooter {
     private server trigger = null;
     private double shooterSpeed = 1;
     private boolean triggerIsReset = false;
+    private ElapsedTime shotTimer = null;
+    Shooter (){
 
-    Shooter (DcMotor leftShooter, DcMotor rightShooter, Servo trigger){
-        leftShootMotor = leftShooter;
-        rightShootMotor = rightShooter;
-        shootTrigger = trigger;
-        setMotorSpeed(0)
-        resetTrigger()
+      leftShootMotor = hardwareMap.dcMotor.get("leftShootMotor");
+      rightShootMotor = hardwareMap.dcMotor.get("rightShootMotor");
+      shootTrigger = hardwareMap.servo.get("shootTrigger");
+      shotTimer = new ElapsedTime)();
+      setMotorSpeed(0)
+      resetTrigger()
     }
 
     public void setMotorSpeed(double speed){
         //set the shooter motor speeds
+
         if (shooterSpeed != speed) {
-          shooterSpeed = speed;
-          leftShootMotor.setMotorSpeed(speed);
-          rightShootMotor.setMotorSpeed(speed);
+          shooterSpeed = speed
+          if (shooterSpeed > 1.0) {shooterSpeed = 1.0}
+          if (shooterSpeed < 0.0) {shooterSpeed = 0.0}
+          leftShootMotor.setMotorSpeed(shooterSpeed);
+          rightShootMotor.setMotorSpeed(shooterSpeed);
         }
     }
 
-    public void pullTrigger(){
+    private void pullTrigger(){
         //moves the trigger to the shoot position
         if (triggerIsReset) {
+          shotTimer.reset()
           shootTrigger.setPosition(settings.posTriggerShoot);
           triggerIsReset = false;
         }
     }
 
-    public void resetTrigger(){
+    private void resetTrigger(){
         //moves the trigger to the reset position
         if (triggerIsReset == false) {
             triggerIsReset = true;
             shootTrigger.setPosition(settings.posTriggerReset);
         }
+    }
+
+    public void shoot(){
+      if (triggerIsReset){
+        pullTrigger()
+      }
+    }
+
+    public boolean isShotDone(){
+      //This is to be called repeatedly to keep the shot progressing
+      if (shotTimer.time() > )
+
+      return false;
+
+    }
+
+
+
+    private boolean isTriggerinPosition(double setPosition, double tol){
+      return false;
     }
 
     public void adjustSpeed(){
