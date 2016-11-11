fb6e70807fcf38c5febca5f844ca27bafb31674a
WhitmoreLakeTroBots <wlhsroboticsteam@gmail.com>
2016-11-10 20:09:09 -0500

    small change for a test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Settings.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Settings.java
@@ -4,6 +4,8 @@ package org.firstinspires.ftc.teamcode;
  * Created by mg15 on 10/14/2016.
  */
 
+//test small change
+
 public class Settings {
     public static double normalDriveSpeed = 0.5;
     public static int stageBlueCorner1Forward = 1;
